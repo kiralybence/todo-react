c91f93e52ba49dbbcbd1bca3bf5a02fba4822c86
Bence Király <kiralybencepecs@gmail.com>
2022-06-07 01:01:48 +0200

    Fresh build

--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import listReducer from '../components/List/listSlice';
-
-export const store = configureStore({
-    reducer: {
-        list: listReducer,
-    },
-});
